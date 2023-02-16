715514d0b8dfe07553fb097b269c71165df62eb5
Matthew Antirix <matthew.antirix@gmail.com>
2023-02-16 09:57:50 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import {cz} from "./language_cz"
+import {en} from "./language_en"
+import "./basic.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Language module //
+
+let language
+let languageSwitch = "cz"
+    if (languageSwitch === "cz") {
+        language = cz
+    } else {
+        language = en
+    }
+
+    
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Main function ///////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+export const ToDoApp = () => {
+
+    return (
+      <>
+        xx
+      </>
+    )
 }
 
-export default App;
+
+
+
+
